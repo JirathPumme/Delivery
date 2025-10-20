616533ca4c1981dc7f35ac00215ab2407e503c26
Asuka <66011212159@msu.ac.th>
2025-10-20 19:13:39 +0700

    fix: Secure API Key

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
   ),
   title: 'Delivery',
   // home: const MyApp(),
-  initialRoute: '/google_map',
+  initialRoute: '/rider_main_screen',
   routes: {
     '/splash': (context) => const Splash_Page(),
     '/login': (context) => const LoginPage(),
